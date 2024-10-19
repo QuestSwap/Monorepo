5f262822495881a90bf067a7502516e0c0b0415a
Sergey Nesterov <sergioreklamplay@gmail.com>
2024-10-19 18:14:43 +0300

    Fixed all errors and linted code

--- hook/src/QuestHook.sol
+++ hook/src/QuestHook.sol
@@ -9,29 +9,51 @@ import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {BalanceDelta, BalanceDeltaLibrary} from "v4-core/src/types/BalanceDelta.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
-import {TaskData} from "./types/TaskData.sol";
-import {PairVolume} from "./types/PairVolume.sol";
+import {TaskData, TaskDataArguments} from "../types/TaskData.sol";
+import {PairVolume} from "../types/PairVolume.sol";
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract QuestHook is BaseHook, Ownable {
     using PoolIdLibrary for PoolKey;
-    using BalanceDeltaLibrary for BalanceDelta global;
-
-    TaskManger public taskManager;
+    using BalanceDeltaLibrary for BalanceDelta;
 
     mapping(PoolId => mapping(address user => uint256 count)) private swapsCount;
     mapping(PoolId => mapping(address user => PairVolume volume)) private totalVolume;
 
-    mapping(TaskData task => mapping(address user => uint256 count)) private tasksSwapCount;
-    mapping(TaskData task => mapping(address user => PairVolume volume)) private tasksSwapVolume;
+    mapping(PoolId => TaskData task) private poolTask;
 
-    mapping(TaskData task => address[] eligibleUsers) private taskEligibleUsers;
+    mapping(uint32 taskId => mapping(address user => uint256 count)) private tasksSwapCount;
+    mapping(uint32 taskId => mapping(address user => PairVolume volume)) private tasksSwapVolume;
+
+    mapping(uint32 taskId => address[] eligibleUsers) private taskEligibleUsers;
+
+    uint32 public lastTaskId = 0;
 
     constructor(IPoolManager _poolManager, address initialOwner) BaseHook(_poolManager) Ownable(initialOwner) {}
 
-    function createTask(PoolId poolId, TaskData taskData) external;
-    function getTask(PoolId poolId) view returns (bool, TaskData);
+    function createTask(PoolId poolId, TaskDataArguments calldata taskData) external {
+        lastTaskId += 1;
+        TaskData memory newTaskData = TaskData({
+            taskId: lastTaskId,
+            poolId: taskData.poolId,
+            rewardAmount: taskData.rewardAmount,
+            expectedVolume: taskData.expectedVolume,
+            expectedTxs: taskData.expectedTxs,
+            startTime: taskData.startTime,
+            endTime: taskData.endTime,
+            isCompleted: taskData.isCompleted,
+            maxParticantsNumber: taskData.maxParticantsNumber,
+            particantsNumber: taskData.particantsNumber
+        });
+
+        poolTask[poolId] = newTaskData;
+    }
+
+    function getTask(PoolId poolId) public view returns (bool, TaskData memory) {
+        TaskData memory task = poolTask[poolId];
+        return (task.taskId != 0, task);
+    }
 
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
@@ -56,45 +78,55 @@ contract QuestHook is BaseHook, Ownable {
         return x >= 0 ? x : -x;
     }
 
-    function isValidTask(TaskData task) private pure returns (bool) {
+    function isValidTask(TaskData memory task) private view returns (bool) {
         return !task.isCompleted || task.particantsNumber <= task.maxParticantsNumber || 
             (block.timestamp >= task.startTime && block.timestamp <= task.endTime);
     }
 
-    function _completeTask(address particant, TaskData task) {
-        activeTask.particantsNumber += 1;
-        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+    function isCompletedTask(TaskData memory task, address sender) private view returns (bool) {
+        PairVolume memory pairVolume = tasksSwapVolume[task.taskId][sender];
+        (uint256 volume0, uint256 volume1) = (pairVolume.token0Volume, pairVolume.token1Volume);
+
+        return volume0 + volume1 >= task.expectedVolume && 
+            tasksSwapCount[task.taskId][sender] >= task.expectedTxs;
+    }
+
+    function _completeTask(address payable particant, uint256 rewardAmount) private {
+        (bool sent, ) = particant.call{value: rewardAmount}("");
         require(sent, "Failed to send Ether");
     }
 
     function afterSwap(
         address sender,
         PoolKey calldata key,
-        IPoolManager.SwapParams calldata swapParams,
+        IPoolManager.SwapParams calldata,
         BalanceDelta delta,
-        bytes calldata hookData
-    ) external returns (bytes4, int128) {
+        bytes calldata
+    ) external override returns (bytes4, int128) {
         PoolId poolId = key.toId();
 
         uint256 token0Volume = uint256(abs(delta.amount0()));
         uint256 token1Volume = uint256(abs(delta.amount1()));
-        totalVolume[poolId][sender] += (token0Volume, token1Volume);
+        totalVolume[poolId][sender].token0Volume += token0Volume;
+        totalVolume[poolId][sender].token1Volume += token1Volume;
+
         swapsCount[poolId][sender] += 1;
 
-        (bool taskExists, TaskData activeTask) = taskManager.getTask();
+        (bool taskExists, TaskData memory activeTask) = getTask(poolId);
         if (taskExists) {
             if (!isValidTask(activeTask)) {
                 return (BaseHook.afterSwap.selector, 0);
             }
 
-            tasksSwapCount[activeTask][sender] += 1;
-            tasksSwapVolume[activeTask][sender] += (token0Volume, token1Volume);
+            tasksSwapCount[activeTask.taskId][sender] += 1;
+            tasksSwapVolume[activeTask.taskId][sender].token0Volume += token0Volume;
+            tasksSwapVolume[activeTask.taskId][sender].token1Volume += token1Volume;
 
             if (
-                tasksSwapVolume[activeTask][sender] >= activeTask.expectedVolume && 
-                tasksSwapCount[activeTask][sender] >= activeTask.expectedTxs
+                isCompletedTask(activeTask, sender)
             ) {
-                
+                activeTask.particantsNumber += 1;
+                _completeTask(payable(sender), activeTask.rewardAmount);
             }
         }
 
--- hook/types/TaskData.sol
+++ hook/types/TaskData.sol
@@ -3,12 +3,26 @@ pragma solidity ^0.8.26;
 
 import {PoolId} from "v4-core/src/types/PoolId.sol";
 
-struct TaskData {
+struct TaskDataArguments {
     PoolId poolId;
     uint256 rewardAmount;
     uint256 expectedVolume;
     uint256 expectedTxs;
     uint256 startTime;
+    uint256 endTime;
+    bool isCompleted;
+    uint16 maxParticantsNumber;
+    uint16 particantsNumber;
+}
+
+struct TaskData {
+    uint32 taskId;
+    PoolId poolId;
+    uint256 rewardAmount;
+    uint256 expectedVolume;
+    uint256 expectedTxs;
+    uint256 startTime;
+    uint256 endTime;
     bool isCompleted;
     uint16 maxParticantsNumber;
     uint16 particantsNumber;
